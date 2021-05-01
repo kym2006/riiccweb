a7fd7b744f807a2a81aca76937ed4c56c086bc7b
kym2006 <kangyiming2006@gmail.com>
2021-05-01 21:21:23 +0800

    more things to showcase

--- script.js
+++ script.js
@@ -14,3 +14,36 @@ function searchByAuthor() {
         }
     }
 }
+function searchByGenre() {
+    var x = document.getElementById("searchByGenre");
+    tar = x.value
+    tar = tar.toLowerCase()
+    things=document.getElementById("containerofshowcase")
+    for(let i = 0; i < things.childNodes.length; i++) {
+        let card = things.childNodes[i];
+        if(card.nodeName=="DIV") {
+            let author=card.childNodes[1].childNodes[3].childNodes[3].childNodes[5].innerHTML
+            author=author.toLowerCase()
+            console.log(author)
+            if(!author.includes(tar))card.style.display="none"
+            if(author.includes(tar))card.style.display="block"
+        }
+    }
+}
+
+function searchByYear() {
+    var x = document.getElementById("searchByYear");
+    tar = x.value
+    tar = tar.toLowerCase()
+    things=document.getElementById("containerofshowcase")
+    for(let i = 0; i < things.childNodes.length; i++) {
+        let card = things.childNodes[i];
+        if(card.nodeName=="DIV") {
+            let author=card.childNodes[1].childNodes[3].childNodes[3].childNodes[3].innerHTML
+            author=author.toLowerCase()
+            console.log(author)
+            if(!author.includes(tar))card.style.display="none"
+            if(author.includes(tar))card.style.display="block"
+        }
+    }
+}
